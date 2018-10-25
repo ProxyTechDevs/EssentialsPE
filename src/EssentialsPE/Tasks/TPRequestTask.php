23cc230ba9c21db461899260b760f28f6633c273
ProxyTechDevs <44120635+ProxyTechDevs@users.noreply.github.com>
2018-10-25 18:59:08 -0400

    Update TPRequestTask.php

--- src/EssentialsPE/Tasks/TPRequestTask.php
+++ src/EssentialsPE/Tasks/TPRequestTask.php
@@ -1,9 +1,6 @@
 <?php
-
 declare(strict_types = 1);
-
 namespace EssentialsPE\Tasks;
-
 use EssentialsPE\BaseFiles\BaseTask;
 use EssentialsPE\BaseFiles\BaseAPI;
 use pocketmine\Player;
@@ -27,8 +24,8 @@ class TPRequestTask extends BaseTask{
      */
     public function onRun(int $currentTick): void{
         if($this->requester instanceof Player && $this->requester->isOnline()) {
-            $this->getAPI()->getServer()->getLogger()->debug(TextFormat::YELLOW . "Running EssentialsPE's TPRequestTask");
+            $this->getAPI()->getServer()->getLogger()->debug("");
             $this->getAPI()->removeTPRequest($this->requester);
         }
     }
-} 
+}
